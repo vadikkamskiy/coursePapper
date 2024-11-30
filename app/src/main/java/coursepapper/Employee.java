437d7e78054b3b2404e19954edcb7beece7718f0
vadikkamskiy <vadikkamskiy@gmail.com>
2024-11-30 19:31:57 +0300

    minimum requirements

--- app/src/main/java/coursepapper/Employee.java
+++ app/src/main/java/coursepapper/Employee.java
@@ -1,64 +1,43 @@
 package main.java.coursepapper;
 
-import java.util.Scanner;
-
 public class Employee{
-    private int id;
-    private String name;
+
+    private static int idGenerator=1;
+
+    private final int id;
+    private final String name;
     private int unit;
     private int salary; 
     public Employee(String n, int u,int s){
+        id = idGenerator++;
         name = n;
         unit = u;
         salary = s;
     }
-    @Override
-    public String toString(){
-        return id+" " + name+" " + unit+" " + salary;
-    }
-    // сетеры + гетеры
-    public void setID(int i){
-        id = i;
-    }
-    public int getID(){
+    public int getId(){
         return id;
-    }
-    public void setName(String n){
-        name = n;
     }
     public String getName(){
         return name;
     }
-    public void setSalary(int s){
-        salary = s;
+    public int getUnit(){
+        return unit;
     }
     public int getSalary(){
         return salary;
     }
+    public void setSalary(int s){
+        this.salary = s;
+    }
     public void setUnit(int u){
-        unit = u;
-    }
-    public int getUnit(){
-        return unit;
+        this.unit = u;
     }
 
-    //метод для удаления сотрудника
-
-    public void emptyEmployee(){
-        name = "empty";
-        unit = 0;
-        salary = 0;
-    }
     @Override
-    public boolean equals(Object other) {
-        if (this.getClass() != other.getClass()) {
-            return false;
-        }
-        Employee e = (Employee) other;
-        return name.equals(e.name) && id == e.id;    
-    }
-    @Override
-    public int hashCode(){
-        return java.util.Objects.hash(id + name + salary);
+    public String toString(){
+        return "id - "+id +  " "+ 
+        "Name - " + name + " "+ 
+        "unit - " + unit + " "+ 
+        "salary - " + salary +" " ;
     }
 }
